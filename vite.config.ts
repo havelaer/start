d6015203d3ea0c6e74ae032de65a37484db73934
Max Meinders <max@havelaer.nl>
2025-08-01 15:59:15 +0200

    chore: use renamed plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import ssr from "@havelaer/vite-ssr/plugin";
+import ssr from "@havelaer/vite-plugin-ssr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
